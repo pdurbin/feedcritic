e49febcdf43c1a4c587295f629a5b77ee6819b8e
Philip Durbin <philipdurbin@gmail.com>
2017-03-14 15:57:31 -0400

    add button for latest episodes

--- feedcritic.js
+++ feedcritic.js
@@ -2,16 +2,25 @@ function init() {
     loadJSON(function(response) {
         // Parse JSON string into object
         var mydata = JSON.parse(response);
-        document.getElementById("result").innerHTML = get_result(mydata);
+        document.getElementById("result").innerHTML = get_result_podcasts(mydata);
+    },
+    "podcasts.json");
+}
 
-    });
+function showLatest() {
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        var mydata = JSON.parse(response);
+        document.getElementById("result").innerHTML = get_result_latest(mydata);
+    },
+    "latest.json");
 }
 
 function clearResult() {
     document.getElementById("result").innerHTML = "";
 }
 
-function get_result(data) {
+function get_result_podcasts(data) {
     var r = data.length + " feeds <ul>";
     for (var i = 0; i < data.length; ++i) {
         r = r + "<li style=\"list-style: none;\"><a href=\"" + data[i]["feed"] + "\"><img src=\"rssfeed.svg\" height=35px></a> <a href=\"" + data[i]["url"] + "\">" + data[i]["title"] + "</a> " + "(updated " + data[i]["Latest"] + "): " + data[i]["description"];
@@ -20,12 +29,21 @@ function get_result(data) {
     return r + "</ul>";
 }
 
+function get_result_latest(data) {
+    var r = data.length + " episodes <ul>";
+    for (var i = 0; i < data.length; ++i) {
+        r = r + "<li><a href=\"" + data[i]["Link"] + "\">" + data[i]["Title"] + "</a> (" + data[i]["Podcast"] + ")";
+        r = r + "</li>";
+    }
+    return r + "</ul>";
+}
+
 // https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
-function loadJSON(callback) {
+function loadJSON(callback, jsonFile) {
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'podcasts.json', true);
+    xobj.open('GET', jsonFile, true);
     xobj.onreadystatechange = function() {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
